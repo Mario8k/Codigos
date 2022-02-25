5993a8a58f54dc270da191c21a6e64439899462d
Mario Raul Naranjo <mario8k@gmail.com>
2022-02-24 21:47:50 -0300

    Clases1.cs

---
+++ Clases1.cs
@@ -0,0 +1,10 @@
+class Hola
+{
+public static void Main()
+{
+/* WriteLine es un método de la clase Console,
+la cual se encuentra en el namespace System.
+Estas líneas son tambien un comentario*/
+System.Console.WriteLine("Hola Mundo");
+}
+}
